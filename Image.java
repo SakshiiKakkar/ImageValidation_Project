3afab9787f0f289620a3b0bffb1f7873411bfcb5
SakshiiKakkar <sakshi.kakkar.906@gmail.com>
2026-05-27 17:57:16 +0530

    Image.java

--- Image.java
+++ Image.java
@@ -1,1 +1,42 @@
 
+package models;
+
+public class Image {
+
+    private int id;
+
+    private String title;
+
+    private String url;
+  
+    public Image() {}
+  
+    public Image(String title,String url ) {
+        this.title = title;
+        this.url = url;
+    }
+  
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
